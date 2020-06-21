0a96c6f7d4bca861f84ce128b4d16602b43919e1
Brayan Llanos <brayan0428@gmail.com>
2020-06-20 21:21:14 -0500

    Updates

---
+++ precache-manifest.61fa006d92d1639ec0773f4073ba1d32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded4eba65f32ef656a297d1926eb66c0",
+    "url": "/gifs-app/index.html"
+  },
+  {
+    "revision": "9d3d96e7b5c3e663caa9",
+    "url": "/gifs-app/static/css/main.4e695806.chunk.css"
+  },
+  {
+    "revision": "aa8b4ec0d42a8d346279",
+    "url": "/gifs-app/static/js/2.e273df15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gifs-app/static/js/2.e273df15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3d96e7b5c3e663caa9",
+    "url": "/gifs-app/static/js/main.c49a318b.chunk.js"
+  },
+  {
+    "revision": "5770f5948b546f4e82ac",
+    "url": "/gifs-app/static/js/runtime-main.22e9539a.js"
+  }
+]);
